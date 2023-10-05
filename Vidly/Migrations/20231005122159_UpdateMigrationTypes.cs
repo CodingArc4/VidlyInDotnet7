44416d86e4b82bf35540f5cc3fbf3471fdb9ba6f
CodingArc4 <kmunib10@outlook.com>
2023-10-05 17:29:15 +0500

    Completed exercise of the course by adding new property in membership type and updating it, then displaying membership names in view

---
+++ Vidly/Migrations/20231005122159_UpdateMigrationTypes.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vidly.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateMigrationTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE Membershiptype SET Name = 'PayAsYouGo' WHERE Id = 1");
+            migrationBuilder.Sql("UPDATE Membershiptype SET Name = 'Monthly' WHERE Id = 2");
+            migrationBuilder.Sql("UPDATE Membershiptype SET Name = 'Quaterly' WHERE Id = 3");
+            migrationBuilder.Sql("UPDATE Membershiptype SET Name = 'Annual' WHERE Id = 4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
