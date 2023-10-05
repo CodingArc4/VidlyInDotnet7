a32624007a66bcedecb928fdcbf8ea7368929d55
CodingArc4 <kmunib10@outlook.com>
2023-10-05 15:15:47 +0500

    Add project files.

---
+++ Vidly/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Vidly.Models;
+
+namespace Vidly.Data
+{
+    public class ApplicationDbContext: IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
---
+++ Vidly/ViewModels/RandomMovieViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Vidly.Models;
+
+namespace Vidly.ViewModels
+{
+    public class RandomMovieViewModel
+    {
+        public Movie movie { get; set; }
+        public List<Customer> customers { get; set; }
+    }
+}
