adb7ed5eae3ee128dca6070ff9431d34c000c975
CodingArc4 <kmunib10@outlook.com>
2023-10-10 15:29:51 +0500

    fix minor issues

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -9,7 +9,9 @@ namespace Vidly.Models
         [Required]
         public string Name { get; set; }
 
-        public Genre Genre { get; set; }
+        public Genre? Genre { get; set; }
+
+        [Display(Name = "Genre")]
         public byte GenreId { get; set; }
 
         public DateTime DateAdded { get; set; }
